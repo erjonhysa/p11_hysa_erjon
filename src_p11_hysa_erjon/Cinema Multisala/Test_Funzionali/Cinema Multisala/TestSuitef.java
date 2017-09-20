c900ff0338ccf1f4b01a691ea68c033264bde7bf
erjonhysa <erjon.hysa@hotmail.it>
2017-09-20 16:48:04 +0200

    Add files via upload

---
+++ src_p11_hysa_erjon/Cinema Multisala/Test_Funzionali/Cinema Multisala/TestSuitef.java
@@ -0,0 +1,12 @@
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+	
+@RunWith(Suite.class)
+@Suite.SuiteClasses({UC1.class,UC14.class,UC2.class,UC3.class,
+	UC4.class,UC5.class,UC6.class,UC7.class,UC8.class,UC9.class})
+
+public class TestSuitef {
+	
+
+}
+
